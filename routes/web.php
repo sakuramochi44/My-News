f410789b5f8d7a094ce936321684bd747f9bdbc2
Aina Muraki <sakurasakusakuramochi>
2020-04-15 11:46:39 +0000

    laravel課題11

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//以下を追記
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+/*「http://XXXXXX.jp/XXX というアクセスが来たときに、
+AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。*/
+
+Route::group(['prefix' => 'admin'],  function() {
+ Route::get('XXX','Admin \AAAController@bbb');
+});
+
+/*【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを
+追加しました。web.phpを編集して、
+admin/profile/create にアクセスしたら ProfileController の add Action に、
+admin/profile/edit にアクセスしたら ProfileController の edit Action に
+割り当てるように設定してください。*/
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});

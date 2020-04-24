e101df342b61260c673bed84e8e752e43bd80919
Aina Muraki <sakurasakusakuramochi>
2020-04-24 14:22:04 +0000

    larabel 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -24,6 +24,54 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                       <ul>
+                           @foreach($errors->all() as $e)
+                             <li>{{ $e }}</li>
+                           @endforeach
+                       </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                          <div class="custum-control custum-radio custom-control-inline">
+                              <input type="radio" name="gender" value="女性" id="custumRadioInline1" class="custom-control-input" 
+                              @if( old('gender')=='女性') checked="checked" @endif required>
+                              <lavel class="custom-control-label" for="customRadioInline1">女性</lavel>
+                          </div>
+                           <div class="custum-control custum-radio custom-control-inline">
+                              <input type="radio" name="gender" value="男性" id="custumRadioInline1" class="custom-control-input" 
+                              @if( old('gender')=='男性') checked="checked" @endif required>
+                              <lavel class="custom-control-label" for="customRadioInline1">男性</lavel> 
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                         <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <inrut type="submit" class="btn btn-primary" value="更新"></inrut>
+                    </div>
+                    </div>
+                    
+                    </div>
+                </form>
             </div>
         </div>
     </div>
